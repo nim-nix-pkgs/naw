6be7cd78f3113563bc46dd4f106443dfaed93dee
hugosenari <hugosenari@gmail.com>
2022-03-12 04:07:33 -0300

    chore: re index naw

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A glue wrapper to do awk-style text processing with Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."naw-master".url = "path:./master";
+  inputs."naw-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."naw-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
